d6fbbaeccca98f1ea7fd24839bfd819ce2e740bd
Chris <39100258+Filk47473831@users.noreply.github.com>
2020-03-06 13:57:13 +0000

    Update editsettings.php

--- editsettings.php
+++ editsettings.php
@@ -19,8 +19,8 @@ if($authList !== null) { $authList = implode("\n",$authList); }
               <?php
 
               if(isset($_POST['updateApp'])) {
-                shell_exec("..\..\git\bin\git.exe -c http.sslVerify=false reset --hard 2>&1");
-                $outputs = shell_exec("..\..\git\bin\git.exe -c http.sslVerify=false pull https://b47ce1f940d20badca903c57add8be34ab2f6abc@github.com/Filk47473831/ADDog.git 2>&1");
+                shell_exec("..\git\bin\git.exe -c http.sslVerify=false reset --hard 2>&1");
+                $outputs = shell_exec("..\git\bin\git.exe -c http.sslVerify=false pull https://b47ce1f940d20badca903c57add8be34ab2f6abc@github.com/Filk47473831/ADDog.git 2>&1");
                 $outputs = substr($outputs, 89);
                 echo "Check Complete";
                 echo "<p class='small'>$outputs</p>";
