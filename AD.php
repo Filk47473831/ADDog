8e773e3da5615f6d3a3b5115ae057bf4c7868f3d
Chris <39100258+Filk47473831@users.noreply.github.com>
2020-03-10 21:41:37 +0000

    Update AD.php

--- AD.php
+++ AD.php
@@ -550,7 +550,6 @@ public function __construct() {
             global $ds;
             global $settings;
             if($_POST['inputUsername'] !== "" && $_POST['inputPassword'] !== "") {
-              if($settings->Domain !== null) {
                 if(ldap_bind($ds, strtolower($_POST['inputUsername']) . "@" . $settings->Domain, $_POST['inputPassword']) === false) {
                   $errno = ldap_errno($ds);
                   return $errno;
@@ -562,7 +561,6 @@ public function __construct() {
                 } else { return false; }
                 }
             } else { return false; }
-          } else { return false; }
         }
 
         function isLoggedIn() {
