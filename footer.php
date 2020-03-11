c9508a18af3af5ae293e15d70e9b9d19364de1c8
Chris <39100258+Filk47473831@users.noreply.github.com>
2020-03-11 20:07:34 +0000

    1.01

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto unselectable">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Chris Groves 2020</div>
+                            <div class="text-muted">Copyright &copy; Chris Groves 2020 - v1.01</div>
                         </div>
                     </div>
                 </footer>
