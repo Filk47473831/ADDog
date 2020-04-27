e79190fb88754a2f019bc554e6fb5de1bde98eb7
Chris Groves <chris@thegaff.co.uk>
2020-04-27 17:25:43 +0100

    Fix OU Select Template Add

--- addusertemplate.php
+++ addusertemplate.php
@@ -141,7 +141,7 @@
   </main>
 <script>
   $( function() {
-    var users = <?php $AD->updateUsersJSON($data); ?>;
+    var users = [<?php echo $AD->updateUsersJSON($data); ?>];
 
     $("#inputUser").autocomplete({
       minLength: 0,
